4c7e314cb54c7b0de1e4052a1219b85b662370ad
unknown <annap.0103@gmail.com>
2024-09-23 14:12:28 -0300

    criando validacoes para a tela de login e de signup

--- stock-control/src/app/modules/home/home.component.ts
+++ stock-control/src/app/modules/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   loginCard = true;
+
+  loginForm = this.formBuilder.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  })
+
+  signupForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+
+  })
+  constructor(private formBuilder: FormBuilder) {}
+
+  onSubmitLoginForm(): void{
+    console.log('Dados do form de login', this.loginForm.value);
+  }
+  onSubmitSignupForm(): void{
+    console.log('Dados do form de criar conta', this.signupForm.value);
+  }
 }
