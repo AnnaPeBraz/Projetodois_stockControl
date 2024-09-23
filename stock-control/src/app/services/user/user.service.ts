37c857e0e38b4e7b84cc321e5e6a9c3d7a209c0a
unknown <annap.0103@gmail.com>
2024-09-23 14:36:22 -0300

    Criando a autenticacao do usuario

--- stock-control/src/app/services/user/user.service.ts
+++ stock-control/src/app/services/user/user.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { AuthRequest } from 'src/app/models/interfaces/user/auth/AuthRequest';
+import { AuthResponse } from 'src/app/models/interfaces/user/auth/AuthResponse';
 import { SignupUserRequest } from 'src/app/models/interfaces/user/SignupUserRequest';
 import { SignupUserResponse } from 'src/app/models/interfaces/user/SignupUserResponse';
 import { environment } from 'src/environments/environment';
@@ -17,5 +19,9 @@ export class UserService {
     return this.http.post<SignupUserResponse>(
       `${this.API_URL}/user`, requestDatas
     );
+  }
+
+  authUser(requestDatas: AuthRequest): Observable<AuthResponse {
+    return this.http.post<AuthResponse>(`${this.API_URL}/auth`, requestDatas);
   }
 }
