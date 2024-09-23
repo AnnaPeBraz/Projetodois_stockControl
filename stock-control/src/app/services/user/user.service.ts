1dc20fee9a125a696376a25ec88f6960e7186156
unknown <annap.0103@gmail.com>
2024-09-23 14:31:08 -0300

    adicionando metodo de criar usuario

--- stock-control/src/app/services/user/user.service.ts
+++ stock-control/src/app/services/user/user.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { SignupUserRequest } from 'src/app/models/interfaces/user/SignupUserRequest';
+import { SignupUserResponse } from 'src/app/models/interfaces/user/SignupUserResponse';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private API_URL = environment.API_URL;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  signupUser(requestDatas: SignupUserRequest): Observable<SignupUserResponse> {
+    return this.http.post<SignupUserResponse>(
+      `${this.API_URL}/user`, requestDatas
+    );
+  }
 }
