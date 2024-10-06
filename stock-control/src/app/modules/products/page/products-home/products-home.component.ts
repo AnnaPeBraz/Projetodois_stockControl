7c471105d2335a294c4d73a3e73cc0825accd138
unknown <annap.0103@gmail.com>
2024-10-06 02:21:33 -0300

    implementando busca de produtos e a tabela

--- stock-control/src/app/modules/products/page/products-home/products-home.component.ts
+++ stock-control/src/app/modules/products/page/products-home/products-home.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { GetAllProductsResponse } from './../../../../models/interfaces/products/response/GetAllProductsResponse';
+import { ProductsDataTransferService } from './../../../../shared/services/products/products-data-transfer.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { Subject, takeUntil } from 'rxjs';
+import { ProductsService } from 'src/app/services/products/products.service';
 
 @Component({
   selector: 'app-products-home',
   templateUrl: './products-home.component.html',
-  styleUrls: []
+  styleUrls: [],
 })
-export class ProductsHomeComponent {
+export class ProductsHomeComponent implements OnInit, OnDestroy {
+  private readonly destroy$: Subject<void> = new Subject();
+  public productsDatas: Array<GetAllProductsResponse> = [];
 
+  constructor(
+    private productsServices: ProductsService,
+    private productsDataService: ProductsDataTransferService,
+    private router: Router,
+    private messageService: MessageService,
+  ) {}
+
+  ngOnInit(): void {
+    this.getServiceProductsDatas();
+  }
+
+  getServiceProductsDatas() {
+    const productsLoaded = this.productsDataService.getProductsDatas();
+
+    if (productsLoaded.length > 0) {
+      this.productsDatas = productsLoaded;
+    } else this.getAPIProductsDatas();
+  }
+
+  getAPIProductsDatas() {
+    this.productsServices
+      .getAllProducts()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (response) => {
+          if (response.length > 0) {
+            this.productsDatas = response;
+          }
+        },
+        error: (err) => {
+          console.log(err);
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Erro',
+            detail: 'Erro ao buscar produto',
+            life: 2500,
+          })
+          this.router.navigate(['/dashboard'])
+        },
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
--- stock-control/src/app/modules/products/products.module.ts
+++ stock-control/src/app/modules/products/products.module.ts
@@ -18,10 +18,12 @@ import { ConfirmationService } from 'primeng/api';
 import { ProductsHomeComponent } from './page/products-home/products-home.component';
 import { PRODUCTS_ROUTES } from './products.routing';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { ProductsTableComponent } from './components/products-table/products-table.component';
 
 @NgModule({
   declarations: [
-    ProductsHomeComponent
+    ProductsHomeComponent,
+    ProductsTableComponent
   ],
   imports: [
     CommonModule,
